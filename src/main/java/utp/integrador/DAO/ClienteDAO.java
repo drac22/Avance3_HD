42b21666a1e4c370998aa8f93dc23b8fbf0114c4
David Ruben <aronecalle22@gmail.com>
2025-07-03 20:44:33 -0500

    Update ClienteDAO.java

--- src/main/java/utp/integrador/DAO/ClienteDAO.java
+++ src/main/java/utp/integrador/DAO/ClienteDAO.java
@@ -13,7 +13,7 @@ public class ClienteDAO implements IClienteDAO {
 
     //listar
     @Override
-    public Cliente listarClientes() {
+    public List<Cliente> listarClientes() {
         List<Cliente> clientes = new ArrayList<>();
         var sql = "SELECT * FROM tbl_cliente ORDER BY id";
         try (Connection con = Conexion.getConexion()) {
